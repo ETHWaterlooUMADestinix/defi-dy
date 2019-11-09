6d1908aff1c73d0582ac127c4ed7853b8566b288
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-11-09 09:06:39 -0500

    UI updates

--- app/src/components/molecules/AssetRowHeader.js
+++ app/src/components/molecules/AssetRowHeader.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 export default function AssetRowHeader() {
     return (
-        <div class="row bg-eth rounded text-white">
-            <div class="col col-1">
+        <div class="row bg-eth rounded text-white font-weight-bold">
+            <div class="col col-1 p-3">
                 Number
             </div>
-            <div className="col col-1">
+            <div className="col col-1 p-3">
                 Name
             </div>
-            <div className="col col-1">
+            <div className="col col-1 p-3">
                 Category
             </div>
-            <div className="col col-1">
+            <div className="col col-1 p-3">
                 Locked
             </div>
-            <div className="col col-1">
+            <div className="col col-1 p-3">
                 1 Day %
             </div>
         </div>
--- app/src/components/organisms/TheHeader.js
+++ app/src/components/organisms/TheHeader.js
@@ -2,17 +2,13 @@ import React from 'react'
 
 export default function TheHeader() {
     return (
-        <header>
-            <h1>Defi-Dy</h1>
-            <p>Bet against a company, not a stock</p>
-
-            <div class="container text-left">
-                <div class="rounded text-white overflow-hidden d-flex align-items-center pl-5" style={{background: '#1b2638', height: '300px'}}>
-                    <h1 class="ml-5" style={{flex:1, 'font-size': '2rem'}}>
-                        Invest in real usage,<br/>not stocks
-                    </h1>
-                    <img src="/pluto.png" style={{width: '550px', 'margin-right': '-100px', 'margin-top': '14px'}}/>
-                </div>
+        <header class="container text-left">
+            <img src="/logo.png" width={120} class="my-3"/>
+            <div class="rounded text-white overflow-hidden d-flex align-items-center pl-5" style={{background: '#1b2638', height: '300px'}}>
+                <h1 class="ml-5" style={{flex:1, 'font-size': '2rem'}}>
+                    Invest in real usage,<br/>not stocks
+                </h1>
+                <img src="/pluto.png" style={{width: '550px', 'margin-right': '-100px', 'margin-top': '14px'}}/>
             </div>
         </header>
     )
